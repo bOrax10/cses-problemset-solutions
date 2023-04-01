e973033b57f9cf19505bcf30579df75a5cbde135
Atharva Dixit <115524391+bOrax10@users.noreply.github.com>
2023-04-01 19:48:54 +0530

    Add files via upload

---
+++ Introductory Problems/trailing-zeroes.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repb(i,a,b) for(int i=a;i>=b;i--)
+#define endl '\n'
+#define print(x) cout<<x<<endl
+#define printInline(x) cout<<x<<" "
+ 
+#define ll long long
+#define lli long long int
+ 
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define vpi vector<pair<int,int>>
+#define vpll vector<pair<long long,long long>>
+ 
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+#define popb pop_back
+#define mp make_pair
+#define all(v) (v).begin(), (v).end()
+
+int main() {
+	int n;
+	cin>>n;
+	int cnt=0;
+	
+	while(n>1) {
+		cnt+=n/5;
+		n/=5;
+	}
+ 
+	cout<<cnt;
+	return 0;
+}

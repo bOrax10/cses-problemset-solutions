cf1e0d5d3b61659c2886a9354fb1bbd022e8ccc2
Atharva Dixit <115524391+bOrax10@users.noreply.github.com>
2023-04-01 19:42:08 +0530

    Add files via upload

---
+++ Introductory Problems/increasing-array.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repb(i,a,b) for(int i=a;i>=b;i--)
+#define endl '\n'
+#define print(x) cout<<x<<endl
+#define printInline(x) cout<<x<<" "
+ 
+#define ll long long
+#define lli long long int
+ 
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define vpi vector<pair<int,int>>
+#define vpll vector<pair<long long,long long>>
+ 
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+#define popb pop_back
+#define mp make_pair
+#define all(v) (v).begin(), (v).end()
+
+int main() {
+	int n;
+	cin>>n;
+ 
+	int a[n];
+ 
+	for(int i=0; i<n; i++) {
+		cin>>a[i];
+	}
+ 
+	long long int ans = 0;
+ 
+	for(int i=1;i<n;i++) {
+		if(a[i]<a[i-1]) {
+			ans+=a[i-1]-a[i];
+			a[i]=a[i-1];
+		}
+	}
+ 
+	cout<<ans<<endl;
+}
